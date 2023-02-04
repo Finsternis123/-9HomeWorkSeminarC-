10fcc8b6ee5abc1974f65449b0942cc8ac066b3a
Vladislav <zomba4o4ek100@gmail.com>
2023-02-04 15:05:43 +0300

    First task complete

---
+++ Task#1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выполнить с помощью рекурсии.Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+Console.Write("Enter number: ");
+int N  = int.Parse(Console.ReadLine()!);
+Console.WriteLine(PrintNumbers(N,1));
+
+string PrintNumbers(int start, int end)
+{
+    if(start == end) return start.ToString();
+    return (start + " " + PrintNumbers(start - 1, end));
+}
